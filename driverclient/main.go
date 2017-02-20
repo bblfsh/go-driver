d63cc863dd3e1832a3e9509fc63c593c818ff163
Manuel Carmona <manuel@sourced.tech>
2017-02-20 08:34:40 +0100

    Added flags options to driverclient to set language and language version int the request

--- driverclient/main.go
+++ driverclient/main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/src-d/lang-parsers/go/go-driver/msg"
 
-	"flag"
-	"io/ioutil"
-
+	"github.com/jessevdk/go-flags"
 	"github.com/ugorji/go/codec"
 )
 
-func main() {
-	var file string
-	flag.StringVar(&file, "f", "", "file to get the AST")
-	flag.Parse()
+type options struct {
+	File            string `short:"f" long:"file" description:"Source code file" required:"true"`
+	Language        string `short:"l" long:"language" description:"File's source code language" default:""`
+	LanguageVersion string `short:"v" long:"version" description:"File's source code language version" default:""`
+}
 
-	if file == "" {
-		log.Fatal("file didn't pass as an argument")
+func main() {
+	var opt options
+	parser := flags.NewParser(&opt, flags.Default)
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
 	}
 
-	f, err := os.Open(file)
+	f, err := os.Open(opt.File)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +41,9 @@ func main() {
 	var mpHandle codec.MsgpackHandle
 	mpEnc := codec.NewEncoder(os.Stdout, &mpHandle)
 	mpEnc.MustEncode(&msg.Request{
-		Action:  msg.ParseAst,
-		Content: string(source),
+		Action:          msg.ParseAst,
+		Language:        opt.Language,
+		LanguageVersion: opt.LanguageVersion,
+		Content:         string(source),
 	})
 }
