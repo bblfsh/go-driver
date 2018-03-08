340b4fd627d14c8f19797090d3d3667247bb3e25
Juanjo Alvarez <juanjo@sourced.tech>
2018-03-08 17:36:49 +0100

    Remove // and /*..*/ from comments' tokens
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- driver/normalizer/tonode.go
+++ driver/normalizer/tonode.go
@@ -1,6 +1,8 @@
 package normalizer
 
 import (
+	"strings"
+
 	"gopkg.in/bblfsh/sdk.v1/uast"
 )
 
@@ -17,5 +19,19 @@ var ToNode = &uast.ObjectToNode{
 	},
 	PromotedPropertyStrings: map[string]map[string]bool{
 		"BinaryExpr": {"Op": true},
+	},
+
+	Modifier: func(n map[string]interface{}) error {
+		// Remove // and /*...*/ from comment nodes' tokens
+		if t, ok := n["type"].(string); ok && t == "Comment" {
+			if text, ok := n["Text"].(string); ok {
+				if strings.HasPrefix(text, "//") {
+					n["Text"] = text[2:];
+				} else if strings.HasPrefix(text, "/*") {
+					n["Text"] = text[2:len(text)-2]
+				}
+			}
+		}
+		return nil
 	},
 }
