87a712f65e12b154d1b31bacd45f5d2408c012b5
Juanjo Alvarez <juanjo@sourced.tech>
2018-03-15 13:57:52 +0100

    Fixes from review
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>
    
    Added forgotten files
    
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- fixtures/u2_class_method_qualifiers.go
+++ fixtures/u2_class_method_qualifiers.go
@@ -1,6 +1,5 @@
 package fixtures
 
 type Testcls1 struct{}
-
-func (f testcls1) Testfnc1() {}
-func (f Testcls2) Testfnc1() {}
+func (f testcls1) testfnc1() {}
+func (f Testcls1) Testfnc1() {}
--- fixtures/u2_type_typedef.go
+++ fixtures/u2_type_typedef.go
@@ -1,4 +1,4 @@
 package fixtures
 
 type Testtype1 struct {}
-type Testtype2 = Testtype1
+type Testtype2 Testtype1
