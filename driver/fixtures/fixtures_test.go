b2fad1bbc71e337afc4b65355e3a0bc1a96f00ca
Denys Smirnov <denys@sourced.tech>
2018-04-03 17:56:22 +0200

    move helpers to sdk and use some new helpers from it
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

---
+++ driver/fixtures/fixtures_test.go
@@ -0,0 +1,35 @@
+package fixtures
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/bblfsh/go-driver/driver/golang"
+	"github.com/bblfsh/go-driver/driver/normalizer"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver/fixtures"
+)
+
+const projectRoot = "../../"
+
+var Suite = &fixtures.Suite{
+	Lang: "go",
+	Ext:  ".go",
+	Path: filepath.Join(projectRoot, fixtures.Dir),
+	NewDriver: func() driver.BaseDriver {
+		return golang.NewDriver()
+	},
+	Transforms: driver.Transforms{
+		Native: normalizer.Native,
+		Code:   normalizer.Code,
+	},
+	BenchName: "json",
+}
+
+func TestGoDriver(t *testing.T) {
+	Suite.RunTests(t)
+}
+
+func BenchmarkGoDriver(b *testing.B) {
+	Suite.RunBenchmarks(b)
+}
