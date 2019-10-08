20eae6de6ca037f16ec162d09d776eac7325bc87
lwsanty <lwsanty@gmail.com>
2019-10-08 18:38:20 +0300

    UAST node to AST converter This PR provides converter and tests suite based on fixtures included
    NOTE positions support is still not provided thus some test cases are skipped for now
    
    Relates to:
    - https://github.com/src-d/feature-idea/issues/160
    - https://github.com/src-d/feature-idea/issues/157
    
    Signed-off-by: lwsanty <lwsanty@gmail.com>

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Go driver for [Babelfish](https://github.com/bblfsh/bblfshd) ![Driver Status](https://img.shields.io/badge/status-beta-dbd25c.svg) [![Build Status](https://travis-ci.org/bblfsh/go-driver.svg?branch=master)](https://travis-ci.org/bblfsh/go-driver) ![Native Version](https://img.shields.io/badge/go%20version-1.12-aa93ea.svg) ![Go Version](https://img.shields.io/badge/go%20version-1.12-63afbf.svg)
+# Go driver for [Babelfish](https://github.com/bblfsh/bblfshd) ![Driver Status](https://img.shields.io/badge/status-beta-dbd25c.svg) [![Build Status](https://travis-ci.org/bblfsh/go-driver.svg?branch=master)](https://travis-ci.org/bblfsh/go-driver) ![Native Version](https://img.shields.io/badge/go%20version-1.13-aa93ea.svg) ![Go Version](https://img.shields.io/badge/go%20version-1.13-63afbf.svg)
 
 Development Environment
 -----------------------
 
 Requirements:
 - `docker`
-- Go 1.12+
+- Go 1.13+
 
 To initialize the build system execute: `go test ./driver`, at the root of the project. This will generate the `Dockerfile` for this driver.
 
--- driver/golang/golang.go
+++ driver/golang/golang.go
@@ -7,10 +7,10 @@ import (
 	"go/token"
 	"reflect"
 
-	"github.com/opentracing/opentracing-go"
+	"github.com/bblfsh/go-driver/driver/golang/convert"
 
-	"github.com/bblfsh/sdk/v3/uast"
 	"github.com/bblfsh/sdk/v3/uast/nodes"
+	"github.com/opentracing/opentracing-go"
 )
 
 func ParseString(code string) (*ast.File, *token.FileSet, error) {
@@ -27,111 +27,7 @@ func Parse(code string) (nodes.Node, error) {
 	if err != nil {
 		return nil, err
 	}
-	return convValue(reflect.ValueOf(f), fs)
-}
-
-var (
-	scopeType   = reflect.TypeOf((*ast.Scope)(nil))
-	objectType  = reflect.TypeOf((*ast.Object)(nil))
-	nodeType    = reflect.TypeOf((*ast.Node)(nil)).Elem()
-	posType     = reflect.TypeOf(token.Pos(0))
-	uastIntType = reflect.TypeOf(nodes.Int(0))
-)
-
-func convPos(p token.Pos, fs *token.FileSet) uast.Position {
-	if !p.IsValid() {
-		return uast.Position{}
-	}
-	pos := fs.Position(p)
-	return uast.Position{
-		Offset: uint32(pos.Offset),
-		Line:   uint32(pos.Line),
-		Col:    uint32(pos.Column),
-	}
-}
-
-// convValue takes an AST node/value and converts it to a tree of uast types
-// like Object and List. In this case we have a full control of json encoding
-// and can annotate the tree with native AST type names.
-func convValue(v reflect.Value, fs *token.FileSet) (nodes.Node, error) {
-	if !v.IsValid() {
-		return nil, nil
-	}
-	t := v.Type()
-	switch t {
-	case posType:
-		p := convPos(v.Interface().(token.Pos), fs)
-		return p.ToObject(), nil
-	}
-	switch t.Kind() {
-	case reflect.Slice:
-		if v.Len() == 0 {
-			return nil, nil
-		}
-		arr := make(nodes.Array, 0, v.Len())
-		for i := 0; i < v.Len(); i++ {
-			el, err := convValue(v.Index(i), fs)
-			if err != nil {
-				return nil, err
-			}
-			arr = append(arr, el)
-		}
-		return arr, nil
-	case reflect.Struct:
-		m := make(nodes.Object, t.NumField())
-		m[uast.KeyType] = nodes.String(t.Name()) // annotate nodes with type names
-		pos := make(uast.Positions)
-		for i := 0; i < t.NumField(); i++ {
-			f := t.Field(i)
-			fv := v.Field(i)
-			if f.Type == posType {
-				p := convPos(fv.Interface().(token.Pos), fs)
-				pos[f.Name] = p
-				continue
-			} else if f.Type == scopeType || f.Type == objectType {
-				// do not follow scope and object pointers - need a graph structure for it
-				continue
-			}
-			el, err := convValue(fv, fs)
-			if err != nil {
-				return nil, err
-			}
-			m[f.Name] = el
-		}
-		if len(pos) != 0 {
-			m[uast.KeyPos] = pos.ToObject()
-		}
-		return m, nil
-	case reflect.Ptr, reflect.Interface:
-		if v.IsNil() {
-			return nil, nil
-		}
-		o, err := convValue(v.Elem(), fs)
-		if err != nil {
-			return nil, err
-		}
-		if m, ok := o.(nodes.Object); ok && v.Type().Implements(nodeType) {
-			n := v.Interface().(ast.Node)
-			pos := uast.PositionsOf(m)
-			if pos == nil {
-				pos = make(uast.Positions)
-			}
-			pos[uast.KeyStart] = convPos(n.Pos(), fs)
-			pos[uast.KeyEnd] = convPos(n.End(), fs)
-
-			m[uast.KeyPos] = pos.ToObject()
-		}
-		return o, nil
-	}
-	o := v.Interface()
-	if s, ok := o.(interface {
-		String() string
-	}); ok {
-		return nodes.String(s.String()), nil
-	} else if t.ConvertibleTo(uastIntType) {
-		return v.Convert(uastIntType).Interface().(nodes.Int), nil
-	}
-	return uast.ToNode(o)
+	return convert.ValueToNode(reflect.ValueOf(f), fs)
 }
 
 func NewDriver() *Driver {
